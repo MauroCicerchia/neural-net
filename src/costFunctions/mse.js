d90bb4012f4fe41b223f52e8c2f1b5d6c42ea7d6
Mauro Cicerchia <maurocicerchia98@gmail.com>
2019-12-22 21:54:25 -0300

    cost function

--- src/costFunctions/mse.js
+++ src/costFunctions/mse.js
@@ -1,6 +1,8 @@
 import _ from "lodash";
 
-export default (x1, x2) => {
-    const errors = _.zipWith(x1, x2, (x1, x2) => Math.pow(x2 - x1, 2));
-    return _(errors).sum() / errors.length;
-}
+const mean = x => _(x).sum() / x.length;
+
+export default [
+    (x1, x2) => mean(_.zipWith(x1, x2, (x1, x2) => Math.pow(x2 - x1, 2))),
+    (x1, x2) => _.zipWith(x1, x2, (x1, x2) => x2 - x1)
+]
